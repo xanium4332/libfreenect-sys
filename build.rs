c9027941a0fc145342f689a26150503ef68a90d1
James Buckley <xanium4332@gmail.com>
2015-10-04 10:57:15 +0100

    Build libfreenect from source if needed

--- build.rs
+++ build.rs
@@ -1,5 +1,25 @@
 extern crate pkg_config;
+extern crate cmake;
 
 fn main() {
-    pkg_config::find_library("libfreenect").unwrap();
+    // Try and find system libfreenect library. If not, attempt to build from source.
+    let found = pkg_config::Config::new().atleast_version("0.5").find("libfreenect").is_ok();
+
+    if found {
+        return;
+    } else {
+        build_from_source();
+    }
+}
+
+// Perform complete libfreenect build from source.
+fn build_from_source() {
+    let _ = cmake::Config::new("libfreenect")
+        .define("BUILD_CPP", "OFF")
+        .define("BUILD_C_SYNC", "OFF")
+        .define("BUILD_EXAMPLES", "OFF")
+        .define("BUILD_FAKENECT", "OFF")
+        .define("BUILD_REDIST_PACKAGE", "ON")
+        .register_dep("libusb-sys")
+        .build();
 }
